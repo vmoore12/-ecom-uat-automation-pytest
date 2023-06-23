c1ab646a1d261a71434239457a1fec8275ff4719
Vickie Moore <vickie.moore26@gmail.com>
2023-06-23 11:29:44 -0500

    added variables.sh

---
+++ gl_variables.sh
@@ -0,0 +1,6 @@
+export DB_USER=vickie
+export DB_PASSWORD=AutomationIsGreat
+export DB_PORT_OVERRIDE=3309
+export DB_HOST_OVERRIDE=bootcamp.store.supersqa.com
+export DB_DATABASE_OVERRIDE=bootcampstore
+export DB_TABLE_PREFIX_OVERRIDE=wp_
