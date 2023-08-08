a0e6f3f89845faf468ff976a122e0016dd5b7723
Admas Kinfu <supersoftwarequalityassurance@gmail.com>
2023-08-08 21:35:11 +0000

    Update file gl_variables.sh

--- gl_variables.sh
+++ gl_variables.sh
@@ -1,7 +1,2 @@
-export BASE_URL=http://bootcamp.store.supersqa.com
-export DB_USER=vickie
-export DB_PASSWORD=AutomationIsGreat
-export DB_PORT_OVERRIDE=3309
-export DB_HOST_OVERRIDE=bootcamp.store.supersqa.com
-export DB_DATABASE_OVERRIDE=bootcampstore
-export DB_TABLE_PREFIX_OVERRIDE=wp_
+export BASE_URL=http://dev.bootcamp.store.supersqa.com
+
